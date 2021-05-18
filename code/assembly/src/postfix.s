c57ca33f75463e46991fea00541fbfcb28ce1fb7
FarinaChristian <58805490+FarinaChristian@users.noreply.github.com>
2021-05-18 16:46:54 +0200

    Add files via upload

--- code/assembly/src/postfix.s
+++ code/assembly/src/postfix.s
@@ -17,7 +17,7 @@ postfix:
     movl 8(%EBP), %ESI   # ESI = input
     movl 12(%EBP), %EDI  # EDI = output
 
-    xorl %ECX, %ECX
+    xorl %ECX, %ECX 
 
     postfix_controllo:
     cmpb $0,(%ESI, %ECX) #quando arriva alla fine e legge \0
@@ -29,7 +29,7 @@ postfix:
     cmpb $45,(%ESI, %ECX) #quando incontra un -
     je postfix_sottrazione
 
-	cmpb $88,(%ESI, %ECX) #quando incontra un X
+	cmpb $42,(%ESI, %ECX) #quando incontra un *
     je postfix_prodotto
 
 	cmpb $47,(%ESI, %ECX) #quando incontra un /
@@ -43,62 +43,64 @@ postfix:
 
     postfix_addizione:
     call addizione
-    pushl %EAX #salvo il risultato dell'addizione
+    #elimino gli ultimi due operandi della pila
+    popl %EDX
+    popl %EDX 
+    pushl %EAX #salvo il risultato 
     inc %ECX
     jmp postfix_controllo
 
     postfix_sottrazione:
     call sottrazione
-    pushl %EAX #salvo il risultato dell'addizione
+    pushl %EAX #salvo il risultato 
     inc %ECX
     jmp postfix_controllo
 
     postfix_prodotto:
     call prodotto
-    pushl %EAX #salvo il risultato dell'addizione
+    pushl %EAX #salvo il risultato
     inc %ECX
     jmp postfix_controllo
 
     postfix_divisione:
     call divisione
-    pushl %EAX #salvo il risultato dell'addizione
+    pushl %EAX #salvo il risultato
     inc %ECX
     jmp postfix_controllo
 
     #metodo che salva i vaori nella pila
-    postfix_spazio:
+    postfix_spazio: 
     dec %ECX
-    movl (%ESI,%ECX),%EDX
+    movl (%ESI,%ECX),%EDX 
     sub $48,%EDX
-    pushl %EDX
+    pushl %EDX  
     addl $2,%ECX #scorro al prossimo carattere
     jmp postfix_controllo
 
     postfix_incrementa:
     inc %ECX
     jmp postfix_controllo
-
+    
 
     postfix_fine:
     # -----------------
-    # il risultato dell'addizione e' nello stack (49 + 49 = 98, ovvero 'b')
-    popl %EAX  # EAX = 'b'
+    # il risultato dell'operazione e' nello stack
+    popl %EAX 
 
     # EDX non serve piu' quindi ci scrivo gli '1'
     # > in teoria dovrei sapere quanti '1' ho inserito e fare tante pop quante ne servono
-    popl %EDX
-    popl %EDX
+    
 
-    # ora nello stack dovrebbe essere tutto pronto
+    # ora nello stack dovrebbe essere tutto pronto 
     # per resettare i registri al valore originale e fare il return
-
+    
     # devo ancora scrivere sul file in output...
     addb $48,%AL
     movb %AL, (%EDI)       # se non scrivo i primi byte che contengono \0non potro' mai leggere sul file in output cio' che mi aspetto
     movl $1, %ECX          # metto ecx alla fine della stringa
-   	movb $0, (%EDI, %ECX)
+   	movb $0, (%EDI, %ECX)  
 
-    # Ripristina registri usati e esegui return
+    # Riprista registri usati e esegui return
     popl %EDX
     popl %ECX
     popl %ESI

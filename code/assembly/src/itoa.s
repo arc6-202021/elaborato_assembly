c5e2f74ea98760e241ddbebe63f0342616789865
Stefano Zenaro <mariortgasd@hotmail.com>
2021-05-23 17:04:27 +0200

    added itoa at the start of the labels' names

--- code/assembly/src/itoa.s
+++ code/assembly/src/itoa.s
@@ -1,13 +1,13 @@
-.section .data
+# converte un intero in una stringa
 
-is_negative:
+.data
+
+itoa_is_negative:
     .byte 1
 
-.section .text
+.text
 	.global itoa
 
-.type itoa, @function
-# converte un intero in una stringa
 itoa:
 
 	pushl %EBP
@@ -17,26 +17,26 @@ itoa:
     pushl %ECX
     pushl %EDX
 	pushl %EDI
-	
+
 	movl 8(%EBP), %EAX
 	movl 12(%EBP), %EDI
-	movb $1, is_negative # parti dicendo che e' negativo
-                         # (utile quando un programma usa un ciclo:
-                         # ogni volta che la funzione viene richiamata is_negative NON viene impostato a 1 automaticamente)
+	movb $1, itoa_is_negative # parti dicendo che e' negativo
+                              # (utile quando un programma usa un ciclo:
+                              # ogni volta che la funzione viene richiamata itoa_is_negative NON viene impostato a 1 automaticamente)
 
     movl $0, %ecx # azzero il contatore
 
     cmpl $0, %EAX
-    jge continua_a_dividere
+    jge itoa_continua_a_dividere
 
-    movb $0, is_negative
+    movb $0, itoa_is_negative
     neg %EAX
 
-continua_a_dividere:
+itoa_continua_a_dividere:
 
 	cmp $10, %eax       # confronta 10 con il contenuto di %eax
 
-	jge dividi			# salta all'etichetta dividi se %eax >= 10
+	jge itoa_dividi		# salta all'etichetta itoa_dividi se %eax >= 10
 
 	pushl %eax			# salva nello stack il contenuto di %eax
 	inc   %ecx			# incrementa di 1 il valore di %ecx per
@@ -47,36 +47,36 @@ continua_a_dividere:
 
     xorl %EBX, %EBX   # EBX = 0
 
-    movb is_negative, %AL
+    movb itoa_is_negative, %AL
     cmpb $0, %AL
-    jne stampa
+    jne itoa_stampa
     movb $45, (%EDI, %EBX)
     inc %EBX
     inc %ECX
 
-	jmp stampa			# salta all'etichetta stampa
+	jmp itoa_stampa			# salta all'etichetta itoa_stampa
 
 
-dividi:
+itoa_dividi:
 
 	movl  $0, %edx		# carica il numero 0 in %edx
 
 	movl $10, %ebx		# carica il numero 10 in %ebx
 
 	divl  %ebx			# divide per %ebx (10) il numero ottenuto concatenando il contenuto di %edx e %eax
- 
+
 	pushl  %edx			# salva il resto della divisione nello stack
 
 	inc   %ecx			# incrementa il contatore delle cifre salvate nello stack
 
-	jmp	continua_a_dividere
+	jmp	itoa_continua_a_dividere
 
 
-stampa:
+itoa_stampa:
 
 	cmp   %ecx, %ebx		# controlla se ci sono ancora caratteri da stampare
 
-	je fine_itoa		# se %ebx=0 ho stampato tutto salto alla fine della funzione
+	je itoa_fine		# se %ebx=0 ho stampato tutto salto alla fine della funzione
 
 	popl  %eax			# preleva l'elemento da stampare dallo stack
 
@@ -85,9 +85,9 @@ stampa:
 	movb %al,(%EDI,%EBX) # aggiungo la cifra nella stringa
     inc   %ebx			# incrementa di 1 l'indice
 
-	jmp   stampa		# ritorna all'etichetta stampa per stampare
-						# il prossimo carattere. 
-fine_itoa:
+	jmp   itoa_stampa		# ritorna all'etichetta itoa_stampa per stampare
+   						# il prossimo carattere.
+itoa_fine:
     movb $0,(%EDI, %ECX)
     movl %ECX, %EAX  # restituisco la lunghezza del numero
 	popl %EDI

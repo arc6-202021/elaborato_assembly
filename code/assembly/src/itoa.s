6c4ed68d0f308d5a2c961c663477b84211b21268
Stefano Zenaro <mariortgasd@hotmail.com>
2021-05-30 15:40:40 +0200

    standardize itoa

--- code/assembly/src/itoa.s
+++ code/assembly/src/itoa.s
@@ -1,102 +1,115 @@
-# converte un intero in una stringa
+#
+# void itoa(int num, char * output)
+#
+# Questa funzione converte <num> in stringa.
+# La stringa viene scritta in <output>.
+#
+# :param int num: numero da convertire in stringa
+# :param * output: stringa in output
 
 .data
 
-itoa_is_negative:
+itoa_is_negative:  # flag: indica che <num> e' negativo quando vale 0
     .byte 1
 
 .text
     .global itoa
 
 itoa:
-
+    # Memorizza nello stack i valori dei registri usati
     pushl %EBP
     movl %ESP, %EBP
 
+    pushl %EAX
     pushl %EBX
     pushl %ECX
     pushl %EDX
     pushl %EDI
 
-    movl 8(%EBP), %EAX
-    movl 12(%EBP), %EDI
-    movb $1, itoa_is_negative # parti dicendo che e' negativo
-                              # (utile quando un programma usa un ciclo:
-                              # ogni volta che la funzione viene richiamata itoa_is_negative NON viene impostato a 1 automaticamente)
+    # inizia algoritmo per convertire intero in stringa
+    movl 8(%EBP), %EAX            # EAX = <num>
+    movl 12(%EBP), %EDI           # EDI = <output>
+    movb $1, itoa_is_negative     # itoa_is_negative = 1 (<num> e' positivo)
+                                  # (utile quando un programma usa un ciclo:
+                                  # ogni volta che la funzione viene richiamata itoa_is_negative NON viene impostato a 1 automaticamente)
+    movl $0, %ECX                 # ECX = 0 (contatore numero cifre memorizzate nello stack)
 
-    movl $0, %ecx # azzero il contatore
+    cmpl $0, %EAX                 # se EAX >= 0
+    jge itoa_continua_a_dividere  # salta a itoa_continua_a_dividere()
 
-    cmpl $0, %EAX
-    jge itoa_continua_a_dividere
-
-    movb $0, itoa_is_negative
-    neg %EAX
+    movb $0, itoa_is_negative     # itoa_is_negative = 0 (True)
+    neg %EAX                      # EAX = -EAX (cambia segno)
 
 
 itoa_continua_a_dividere:
+    # Richiama itoa_dividi() per dividere per 10 EAX
+    # finche' EAX < 10, poi inserisci nello stack l'ultima
+    # cifra e se il numero e' negativo imposta EDI[0] = '-'
 
-    cmp $10, %eax       # confronta 10 con il contenuto di %eax
+    cmpl $10, %EAX              # se EAX >= 10
+    jge itoa_dividi             # salta a itoa_dividi()
 
-    jge itoa_dividi		# salta all'etichetta itoa_dividi se %eax >= 10
+    # -- tutte le cifre meno l'ultima sono nello stack
 
-    pushl %eax			# salva nello stack il contenuto di %eax
-    inc   %ecx			# incrementa di 1 il valore di %ecx per
-                        # contare quante push eseguo;
-                        # ad ogni push salvo nello stack una cifra
-                        # del numero (a partire da quella meno
-                        # significativa)
+    pushl %EAX                  # salva nello stack EAX, l'ultima cifra
+    incl %ECX                   # ECX++
 
-    xorl %EBX, %EBX     # EBX = 0
+    xorl %EBX, %EBX             # EBX = 0 (usato per scorrere EDI)
 
-    movb itoa_is_negative, %AL
-    cmpb $0, %AL
-    jne itoa_stampa
-    movb $45, (%EDI, %EBX)
-    inc %EBX
-    inc %ECX
+    movb itoa_is_negative, %AL  # AL = itoa_is_negative
+    cmpb $0, %AL                # se AL != 0 (<num> positivo)
+    jne itoa_stampa             # salta a itoa_stampa()
 
-    jmp itoa_stampa			# salta all'etichetta itoa_stampa
+    # numero negativo, metti meno in prima posizione
+    # e sposta gli indici per scorrere EDI
+
+    movb $45, (%EDI, %EBX)      # EDI[EBX] = '-' (EDI[0] = '-')
+    incl %EBX                   # EBX++
+    incl %ECX                   # ECX++
+    jmp itoa_stampa             # salta a itoa_stampa()
 
 
 itoa_dividi:
+    # Ricava l'ultima cifra da EAX (EAX % 10),
+    # la inserisce nello stack e impone EAX /= 10.
 
-    movl  $0, %edx		# carica il numero 0 in %edx
-
-    movl $10, %ebx		# carica il numero 10 in %ebx
-
-    divl  %ebx			# divide per %ebx (10) il numero ottenuto concatenando il contenuto di %edx e %eax
-
-    pushl  %edx			# salva il resto della divisione nello stack
-
-    inc   %ecx			# incrementa il contatore delle cifre salvate nello stack
-
+    movl $0, %EDX       # EDX = 0 (primi 32 bit dividendo)
+    movl $10, %EBX      # EBX = 10 (divisore)
+    divl %EBX           # EAX = EDX:EAX / EBX | EDX = EDX:EAX % EBX (EBX = 10)
+    pushl %EDX          # memorizza cifra nello stack
+    incl %ECX           # ECX++ (incrementa contatore cifre)
     jmp	itoa_continua_a_dividere
 
 
 itoa_stampa:
+    # Tutte le cifre sono nello stack,
+    # questa funzione recupera dallo stack le cifre,
+    # le converte in carattere e le inserisce in EDI
 
-    cmp   %ecx, %ebx		# controlla se ci sono ancora caratteri da stampare
+    cmpl %ECX, %EBX         # se EBX == ECX non ho piu' cifre nello stack
+    je itoa_fine            # salta a itoa_fine()
 
-    je itoa_fine		    # se %ebx=0 ho stampato tutto salto alla fine della funzione
+    popl %EAX               # EAX = cifra nello stack
+    addb $48, %AL           # EAX += 48 (converto intero in cifra)
+    movb %AL, (%EDI, %EBX)  # EDI[EBX] = EAX (aggiungo cifra alla stringa)
+    incl %EBX               # incrementa di 1 l'indice
 
-    popl  %eax			    # preleva l'elemento da stampare dallo stack
-
-    addb  $48, %al	        # sommo 0 al valore in al
-
-    movb %al,(%EDI,%EBX)    # aggiungo la cifra nella stringa
-    inc   %ebx			    # incrementa di 1 l'indice
-
-    jmp   itoa_stampa		# ritorna all'etichetta itoa_stampa per stampare
+    jmp itoa_stampa         # ritorna all'etichetta itoa_stampa per stampare
                             # il prossimo carattere.
 
+
 itoa_fine:
-    movb $0,(%EDI, %ECX)
-    movl %ECX, %EAX  # restituisco la lunghezza del numero
+    # Fine algoritmo: metti \0 a fine stringa,
+    # ripristina i valori dei registri e ritorna al chiamante
+
+    movb $0, (%EDI, %ECX)  # EDI[ECX] = '\0'
+
+    # Ripristina valori originali dei registri usati
+    # e restituisci il controllo al chiamante
     popl %EDI
     popl %EDX
     popl %ECX
     popl %EBX
+    popl %EAX
     popl %EBP
-
     ret
-

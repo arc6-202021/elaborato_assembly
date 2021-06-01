483f4c6903561fef8ae347252bc30dea5096d6ea
Stefano Zenaro <mariortgasd@hotmail.com>
2021-06-01 20:49:24 +0200

    + write_result no check (wait for main.c file)

---
+++ code/assembly/src/write_result_nocheck.s
@@ -0,0 +1,69 @@
+#
+# void write_result(int result, int valid, char * out_array)
+#
+# Scrive in <out_array>:
+# * "Invalid" se valid = 1
+# * <result> se valid = 0
+# > La funzione itoa() si occupa di convertire <result> in stringa e di scriverla in <out_array>
+#
+# :param int result: risultato del calcolo (o valore 'finto' in caso di valid = 1)
+# :param int valid: se uguale a 0 viene scritto <result> in formato stringa, altrimenti viene scritto "Invalid"
+# :param char * out_array: array/stringa su cui scrivere l'output
+
+.data
+
+.text
+
+    .global write_result
+
+write_result:
+    pushl %EBP
+    movl %ESP, %EBP  # imposto esp alla base della pila
+
+    pushl %EAX
+    pushl %EBX
+    pushl %ECX
+    pushl %EDX
+
+    movl $1, %EAX  # EAX = invalid = 1
+
+    # prendo i due parametri del metodo dalla pila ed eseguo il prodotto con segno
+    movl 16(%EBP), %EBX   # EBX = <result>
+    movl 12(%EBP), %ECX   # ECX = <valid>
+    movl 8(%EBP), %EDX    # EDX = <out_array>
+
+    cmpl $1, %ECX                  # se ECX != 1, <result> e' valido
+    jne write_result_valid_result  # salta all'etichetta
+
+    # if valid == 1 (valid = False)
+    movb $73, (%EDX)    # I
+    movb $110, 1(%EDX)  # n
+    movb $118, 2(%EDX)  # v
+    movb $97, 3(%EDX)   # a
+    movb $108, 4(%EDX)  # l
+    movb $105, 5(%EDX)  # i
+    movb $100, 6(%EDX)  # d
+    movb $0, 7(%EDX)    # \0
+
+    jmp write_result_fine
+
+
+write_result_valid_result:
+    # <result> e' valido,
+    # richiamo itoa() per scriverlo
+    pushl %EDX  # parametro <out_array>
+    pushl %EBX  # parametro <result>
+    call itoa   # itoa(result, out_array)
+    popl %EBX
+    popl %EDX
+
+
+write_result_fine:
+    popl %EDX
+    popl %ECX
+    popl %EBX
+    popl %EAX
+
+    popl %EBP
+
+    ret
--- tests/unit_tests/test_write_result.c
+++ tests/unit_tests/test_write_result.c
@@ -72,3 +72,49 @@ int test_write_result() {
     // e il return sara' 1
     return success_flag;
 }
+
+
+/**
+ * Quando non sara' piu' necessario il check, cancellare
+ * l'altra funzione e rinominare questa togliendo "_nocheck" al nome.
+*/
+int test_write_result_nocheck() {
+    int res;               // contiene return di sottrazione()
+    int exp_res;           // contiene risultato atteso
+    int success_flag = 0;  // 0 e' successo
+    char output[255] = {0};
+    char invalid_output[] = "Invalid\0";
+
+    // test valori validi
+    for (int result = -5000; result < 5000; result++) {
+
+        write_result(output, 0, result);
+
+        int length = snprintf(NULL, 0, "%d", result);  // trovo lunghezza necessaria per memorizzare k
+        char* str = (char *) malloc(length + 1 );          // alloco lo spazio necessario per k e per il terminatore
+
+        if (str != NULL) {
+            snprintf(str, length + 1, "%d", result);       // converto k in stringa
+
+            if (strcmp(output, str) != 0) {
+                success_flag = 1;
+                printf("result: %d, valid input returned '%s' instead\n", result, output);
+            }
+        }
+        free(str);
+    }
+
+    // test valori esplicitamente invalidi
+    for (int result = -5000; result < 5000; result++) {
+        write_result(output, 1, result);
+        if (strcmp(output, invalid_output) != 0) {
+            success_flag = 1;
+            printf("result: %d, explicit invalid returned '%s' instead\n", result, output);
+        }
+    }
+
+    // basta che uno solo dei
+    // risultati sia diverso da quello atteso
+    // e il return sara' 1
+    return success_flag;
+}
